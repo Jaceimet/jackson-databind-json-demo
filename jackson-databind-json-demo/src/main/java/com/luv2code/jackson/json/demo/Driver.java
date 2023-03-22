8799a0689d2e1d224dd8560c5035ff76a3b8a400
Jaceimet <84209692+Jaceimet@users.noreply.github.com>
2023-03-21 22:29:11 -0700

    Add full json file functionality and print complex data test successful

--- jackson-databind-json-demo/src/main/java/com/luv2code/jackson/json/demo/Driver.java
+++ jackson-databind-json-demo/src/main/java/com/luv2code/jackson/json/demo/Driver.java
@@ -17,14 +17,18 @@ public class Driver {
 			// read JSON file and map/convert to Java POJO: data/sample-lite.json
 			
 			Student theStudent = 
-					mapper.readValue(new File("data/sample-lite.json"), Student.class);
+					mapper.readValue(new File("data/sample-full.json"), Student.class);
 			
 			
 			// print first name and last name
 			System.out.println("First name = " + theStudent.getFirstName());
 			System.out.println("Last name = " + theStudent.getLastName());
+
+			// print out address: street and city
+			Address tempAddress = theStudent.getAddress();
 			
-			
+			System.out.println("Street = " + tempAddress.getStreet());
+			System.out.println("City = " + tempAddress.getCity());
 			
 		}
 		catch(Exception exc) {
--- jackson-databind-json-demo/src/main/java/com/luv2code/jackson/json/demo/Student.java
+++ jackson-databind-json-demo/src/main/java/com/luv2code/jackson/json/demo/Student.java
@@ -10,6 +10,10 @@ public class Student {
 	
 	private boolean active;
 	
+	private Address address;
+	
+	private String[] languages;
+	
 	public Student() {
 
 	}
@@ -44,6 +48,22 @@ public class Student {
 
 	public void setActive(boolean active) {
 		this.active = active;
+	}
+
+	public Address getAddress() {
+		return address;
+	}
+
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+
+	public String[] getLanguages() {
+		return languages;
+	}
+
+	public void setLanguages(String[] languages) {
+		this.languages = languages;
 	}
 	
 	
